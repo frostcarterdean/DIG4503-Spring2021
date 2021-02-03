0f8022830c7b225c097939e97f7b45af678508e7
frostcarterdean <55201666+frostcarterdean@users.noreply.github.com>
2021-02-03 14:50:20 -0500

    Lab 3

---
+++ lab3/src/App.js
@@ -0,0 +1,11 @@
+import HomePage from './components/HomePage/index.js';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage firstName="Carter"></HomePage>
+    </div>
+  );
+}
+
+export default App;
